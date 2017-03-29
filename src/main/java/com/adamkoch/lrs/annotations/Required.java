ec59b8a07ba2bd782e9b873fee906fc14198c494
Adam Koch <adam@dogtired.org>
2017-03-28 20:25:49 -0500

    Initial commit

---
+++ src/main/java/com/adamkoch/lrs/annotations/Required.java
@@ -0,0 +1,13 @@
+package com.adamkoch.lrs.annotations;
+
+/**
+ * See <a href="https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-About.md#interpret-text-table">Guidelines for
+ * Interpreting Descriptive Text and Tables</a>.
+ *
+ * <p>Created by aakoch on 2017-03-22.</p>
+ *
+ * @author aakoch
+ * @since 1.0.0
+ */
+public @interface Required {
+}
